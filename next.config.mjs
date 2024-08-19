17ebeca229b892d65340a38f07a4a863cb40c78f
Aashish-Elias <aashishjohn2001@gmail.com>
2024-08-19 22:15:13 +0300

    commited the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 // next.config.cjs
-const { authMiddleware } = require('./authMiddleware');
+const { authMiddleware } = require("./authMiddleware");
 
 module.exports = {
   // Other configurations
   middleware: [
     // Include the path to your middleware file
-    { path: '/((?!_next|favicon.ico).*)', handler: require.resolve('./middleware') }
+    {
+      path: "/((?!_next|favicon.ico).*)",
+      handler: require.resolve("./middleware"),
+    },
   ],
 };
